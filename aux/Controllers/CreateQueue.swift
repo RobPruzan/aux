4e4c06993f9cfb3b4bac3edef56a67e47b9ddc71
RobPruzan <rapruzan@buffalo.edu>
2024-05-19 18:54:52 -0400

    use logos for tab bar items, clean up create queue view

--- aux/Controllers/CreateQueue.swift
+++ aux/Controllers/CreateQueue.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class CreateQueue: UIViewController {
+    let mainStack = {
+        let stack = UIStackView()
+
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+
     let createQueueButton = {
         let button = UIButton()
         button.configuration = UIButton.Configuration.filled()
@@ -17,21 +27,52 @@ class CreateQueue: UIViewController {
         button.setTitleColor(.white, for: .normal)
         return button
     }()
-    
+
+    let queueNameInput = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter text here"
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.textColor = UIColor.label
+        textField.backgroundColor = UIColor.systemBackground
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.systemGray4.cgColor
+        textField.clearButtonMode = .whileEditing
+        return textField
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .systemBackground
+        setupMainStack()
+    }
+
+    func setupMainStack() {
+        view.addSubview(mainStack)
+        NSLayoutConstraint.activate([
+            mainStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+        setupQueueNameInput()
         setupCreateQueueButton()
     }
-    
+
+    func setupQueueNameInput() {
+        mainStack.addArrangedSubview(queueNameInput)
+        NSLayoutConstraint.activate([
+            queueNameInput.widthAnchor.constraint(equalToConstant: 225),
+            queueNameInput.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+
     func setupCreateQueueButton() {
-        view.addSubview(createQueueButton)
+        mainStack.addArrangedSubview(createQueueButton)
         createQueueButton.addTarget(self, action: #selector(onCreateQueue), for: .touchUpInside)
         NSLayoutConstraint.activate([
-            createQueueButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            createQueueButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
-            createQueueButton.widthAnchor.constraint(equalToConstant: 150),
+            createQueueButton.widthAnchor.constraint(equalToConstant: 225),
             createQueueButton.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
@@ -43,7 +84,7 @@ class CreateQueue: UIViewController {
             } catch {}
         }
     }
-    
+
     func createQueue() async throws {
         struct Body: Codable {}
         let _ = try await APIManager.shared.post(for: "/queue", body: Body(), responseType: [AuxQueue].self)
--- aux/Controllers/TabBarViewController.swift
+++ aux/Controllers/TabBarViewController.swift
@@ -14,12 +14,24 @@ class TabBarViewController: UITabBarController {
         
         let screen1 = CreateQueue()
         let screen2 = ActiveQueues()
+        
         screen1.title = "Create Queue"
         screen2.title = "Active Queues"
         
         screen1.navigationItem.largeTitleDisplayMode = .always
         screen2.navigationItem.largeTitleDisplayMode = .always
         
+        screen1.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        screen2.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "list.bullet"),
+            selectedImage: UIImage(systemName: "list.bullet.fill")
+        )
         
         let nav1 = UINavigationController(rootViewController: screen1)
         let nav2 = UINavigationController(rootViewController: screen2)
